40358136485488fb75253774443f737fadab8016
Yourizorge <fitmetzorge@outlook.com>
2026-08-12 22:31:15 +0200

    Isolate staging Supabase config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-  window.FMZ_CONFIG = {
-  SUPABASE_URL: "https://hgoygcviutmynaihcvpd.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhnb3lnY3ZpdXRteW5haWhjdnBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMzQ2NDcsImV4cCI6MjA5NjcxMDY0N30.RjyRbPJeWhD-SrSxYX1Iwyr5ty9up_LkcG5qf7nfCCI",
+window.FMZ_CONFIG = {
+  SUPABASE_URL: "https://mokxyyulhfkalopbzd.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_6OiMLMl946arkI71-ylqkQ_EQWL6kKT",
   INVITE_FUNCTION_NAME: "invite-client"
 };
-
