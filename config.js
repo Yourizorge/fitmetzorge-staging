71c097a19825f6684f8827c2c4af1817dbd6880f
Yourizorge <fitmetzorge@outlook.com>
2026-08-13 18:06:51 +0200

    Point staging config to correct Supabase project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.FMZ_CONFIG = {
-  SUPABASE_URL: "https://mokxyyulhfkalopbzd.supabase.co",
+  SUPABASE_URL: "https://mokxyyullfhkfalopbzd.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_6OiMLMl946arkI71-ylqkQ_EQWL6kKT",
   INVITE_FUNCTION_NAME: "invite-client"
 };
